c53ddad15f460df63d23c5504e5b667d8a42f564
Initech <inimfonebong001@gmail.com>
2024-01-11 13:29:26 +0000

    fix pycodestyle

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,5 +3,6 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """ type-annotated function concat that takes a string str1 and a string str2 """
-    return str1 + str2 
+    """ type-annotated function concat that takes
+    a string str1 and a string str2 """
+    return str1 + str2
