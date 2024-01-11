5523b407d677e14592acc6cdddff48d91eee8716
Initech <inimfonebong001@gmail.com>
2024-01-11 13:38:54 +0000

    fix E302 expected 2 blank lines, found 1

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """ module for sum_mixed_list() """
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ type-annotated function sum_mixed_list which takes a list mxd_lst
     of integers and floats and returns their sum as a float.
