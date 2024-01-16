4f16f6ad792354cdb275e8a7d60f22831c759c9e
Initech <inimfonebong001@gmail.com>
2024-01-16 10:46:01 +0000

    task

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Run time for four parallel comprehensions """
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ Run time for four parallel comprehensions """
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.perf_counter()
+    return end - start
