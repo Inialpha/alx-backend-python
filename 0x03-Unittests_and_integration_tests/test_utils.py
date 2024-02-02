2b344a5cc7086011c19e153d91b312e87b0fba54
Initech <inimfonebong001@gmail.com>
2024-02-02 18:15:33 +0000

    Fix E501 line too long (80 > 79 characters)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,7 +31,8 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map_exception(
             self, nested_map: Mapping, path: Sequence) -> None:
-        """ test test case for access_nested_map function with wrong outputs """
+        """ test test case for access_nested_map
+        function with wrong outputs """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
@@ -60,6 +61,7 @@ class TestMemoize(unittest.TestCase):
 
         class TestClass:
             """ test class for momoization """
+
             def a_method(self) -> int:
                 """ test method for momoization """
                 return 42
