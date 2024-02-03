693a022c62bc238d5ad8b7f808df962ba0f0732f
Initech <inimfonebong001@gmail.com>
2024-02-03 09:55:41 +0000

    Add test cases for client.GithubOrgClient.org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" test module for client,py """
+from client import GithubOrgClient
+from typing import Dict
+from unittest import TestCase
+from unittest.mock import Mock, patch
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(TestCase):
+    """ test suite for GithubOrgClient """
+    @parameterized.expand([('google',
+                          {'client': 'google'}), ('abc', {'client': 'abc'})])
+    @patch("client.get_json")
+    def test_org(self, org: str, res: Dict, mock: Mock):
+        """ test org method """
+        mock.return_value = Mock(return_value=res)
+        my_client = GithubOrgClient(org)
+        self.assertEqual(my_client.org(), res)
+        mock.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org))
